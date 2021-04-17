6d3da27e073fd64ab0a2deeb33d4e80c86eb8ead
Willian Aquino <aquino.will@gmail.com>
2021-04-17 18:05:22 -0300

    delete services

--- src/services/organizationMembers.services.js
+++
@@ -1,8 +0,0 @@
-import axios from 'config/axios'
-
-const BASE_URL = '/organizations'
-
-export const getOrganizationMembers = organizationId => {
-  const EDIT_URL = `${BASE_URL}/${organizationId}/organization_members`
-  return axios.get(EDIT_URL)
-}
--- src/services/products.services.js
+++
@@ -1,5 +0,0 @@
-import axios from 'config/axios'
-
-const URL = 'public/products'
-
-export const productsService = () => axios.get(URL)
